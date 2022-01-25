fa4dd33c9e84cc3bdb1c9a76eb84ca403c3894c9
Mario A. Gasca <web@mariogasca.com>
2022-01-25 00:55:55 -0600

    Se añadieron las variables de entorno para la conexion a la base de datos

--- config/Env.php
+++ config/Env.php
@@ -14,25 +14,25 @@ class Env
      * Database host
      * @var string
      */
-    const DB_HOST = 'localhost';
+    const DB_HOST = 'host';
 
     /**
      * Database name
      * @var string
      */
-    const DB_NAME = 'test';
+    const DB_NAME = 'db_name';
 
     /**
      * Database user
      * @var string
      */
-    const DB_USER = 'mgasca93';
+    const DB_USER = 'db_user';
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'DbPL259#';
+    const DB_PASSWORD = 'db_passwd';
 
     /**
      * Show or hide error messages on screen
