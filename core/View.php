2a5f2c04cb0e523b318f2b2b97147985dbec8484
Mario A. Gasca <web@mariogasca.com>
2022-01-26 01:05:22 -0600

    Se elimino el sistema de plantillas de twig

--- core/View.php
+++ core/View.php
@@ -32,23 +32,4 @@ class View
         }
     }
 
-    /**
-     * Render a view template using Twig
-     *
-     * @param string $template  The template file
-     * @param array $args  Associative array of data to display in the view (optional)
-     *
-     * @return void
-     */
-    public static function renderTemplate($template, $args = [])
-    {
-        static $twig = null;
-
-        if ($twig === null) {
-            $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/resources/Views');
-            $twig = new \Twig\Environment($loader);
-        }
-
-        echo $twig->render($template, $args);
-    }
 }
